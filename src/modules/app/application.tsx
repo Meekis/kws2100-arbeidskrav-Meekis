16428e1837ede7d49b382a69714aac7378b59a38
meekis <ivyvivi@hotmail.no>
2025-02-20 14:34:38 +0100

    sivilforsvarsdistrikt og pointmove, krisesenter mangler.

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,24 +1,71 @@
 import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
-import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import { useGeographic } from "ol/proj";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+import { Fill, Stroke, Style, Text } from "ol/style";
+import { FeatureLike } from "ol/Feature";
 
 useGeographic();
 
+function focusStyle(feature: FeatureLike) {
+  return new Style({
+    stroke: new Stroke({ color: "red", width: 2 }),
+    text: new Text({
+      text: feature.getProperties().name,
+      fill: new Fill({ color: "red" }),
+      stroke: new Stroke({ color: "white" }),
+    }),
+  });
+}
+
+const municipalityLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/kws2100-arbeidskrav-Meekis/geojson/beredskapsdepot.geojson",
+    format: new GeoJSON(),
+  }),
+  style: new Style({ stroke: new Stroke({ color: "red", width: 2 }) }),
+});
+
 const map = new Map({
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({ source: new OSM() }),
+    municipalityLayer,
+    new VectorLayer({
+      source: new VectorSource({
+        url: "/kws2100-arbeidskrav-Meekis/geojson/sivilforsvarsdistrikt.geojson",
+        format: new GeoJSON(),
+      }),
+    }),
+  ],
+  view: new View({ center: [10.6, 59.9], zoom: 10 }),
 });
 
 export function Application() {
-  const mapRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<HTMLDivElement>(null);
+  const focusFeatures = useRef<Feature[]>([]);
+
+  function handlePointerMove(e: MapBrowserEvent<MouseEvent>) {
+    for (const feature of focusFeatures.current) {
+      feature.setStyle(undefined);
+    }
+    const features = municipalityLayer
+      .getSource()!
+      .getFeaturesAtCoordinate(e.coordinate);
+    for (const feature of features) {
+      feature.setStyle(focusStyle);
+    }
+    focusFeatures.current = features;
+  }
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
+    map.on("pointermove", handlePointerMove);
   }, []);
-
   return <div ref={mapRef}></div>;
 }
